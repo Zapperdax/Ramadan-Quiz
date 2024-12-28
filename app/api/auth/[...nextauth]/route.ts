312a844cb94c6037ae5b4b027d1ad35736d0a25b
Zapperdax <qa80986@gmail.com>
2024-12-28 18:03:30 +0500

    Fixed google auth for deployment build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,17 @@
-import NextAuth, { NextAuthOptions, Session } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { JWT } from "next-auth/jwt";
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth, { DefaultSession, Session } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { JWT } from "next-auth/jwt"
 
-export const authOptions: NextAuthOptions = {
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+    } & DefaultSession["user"]
+  }
+}
+
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -10,15 +19,22 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }: { session: Session; token: JWT }) {
-      // Ensure session.user is defined before adding user.id
-      if (session.user && token?.sub) {
-        session.user.id = token.sub; // Add user ID to the session
+    async session({
+      session,
+      token,
+    }: {
+      session: Session
+      token: JWT
+      user: Session["user"]
+    }) {
+      if (session.user) {
+        session.user.id = token.sub!
       }
-      return session;
+      return session
     },
   },
-};
+}
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
