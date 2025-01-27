9078153e7b82fac831173f75128fc1cabde86ce2
Zapperdax <qa80986@gmail.com>
2025-01-27 17:44:52 +0500

    Urdu question fix

--- questions/urduQuestions.tsx
+++ questions/urduQuestions.tsx
@@ -3,92 +3,92 @@ const urduQuestions = [
     id: 0,
     question: "Urdu mein 'aap' ka matlab kya hota hai?",
     options: ["This", "That", "You", "Me"],
-    correctOption: "You"
+    correctOption: "You",
   },
   {
     id: 1,
     question: "Urdu mein 'dost' ka matlab kya hai?",
-    options: ["Dushman", "Sathi", "Saathi", "Saath"],
-    correctOption: "Sathi"
+    options: ["Dushman", "Sathi", "Paon", "Samundar"],
+    correctOption: "Sathi",
   },
   {
     id: 2,
     question: "Urdu mein 'pani' kis chiz ko kaha jata hai?",
     options: ["Poti", "Surf Excel", "Tang", "Water"],
-    correctOption: "Water"
+    correctOption: "Water",
   },
   {
     id: 3,
     question: "Urdu mein 'ghar' ka kya matlab hai?",
     options: ["Dost", "House", "Land", "AC"],
-    correctOption: "House"
+    correctOption: "House",
   },
   {
     id: 4,
     question: "Urdu mein 'khana' kis chiz ko kaha jata hai?",
     options: ["Pehnawa", "Kapra", "Khushbu", "Food"],
-    correctOption: "Food"
+    correctOption: "Food",
   },
   {
     id: 5,
     question: "Urdu mein 'mohabbat' ka matlab kya hai?",
     options: ["Dost", "Izzat", "Love", "Dushman"],
-    correctOption: "Love"
+    correctOption: "Love",
   },
   {
     id: 6,
     question: "Urdu mein 'sitaray' kis cheez ko kehte hain?",
     options: ["Gulab", "Phool", "Stars", "Dastan"],
-    correctOption: "Stars"
+    correctOption: "Stars",
   },
   {
     id: 7,
-    question: "Urdu mein 'dost' kis ko kaha jata hai?",
-    options: ["Dushman", "Friend", "Enemy", "Bhagwan"],
-    correctOption: "Friend"
+    question: "Indian Drama me Ahem Ji ka laptop kis ne dhooya?",
+    options: ["Ahem Ji", "Kokila", "Gopi bahu", "Kisi ne bhi nai"],
+    correctOption: "Gopi bahu",
   },
   {
     id: 8,
     question: "Urdu mein 'shukriya' ka matlab kya hai?",
     options: ["Sorry", "Please", "Thank you", "Excuse me"],
-    correctOption: "Thank you"
+    correctOption: "Thank you",
   },
   {
     id: 9,
     question: "Urdu mein 'pyaar' kis cheez ka naam hai?",
     options: ["Dosti", "Love", "Dushman", "Suno"],
-    correctOption: "Love"
+    correctOption: "Love",
   },
   {
     id: 10,
     question: "Urdu mein 'behan' ka matlab kya hai?",
     options: ["Sister", "Brother", "Friend", "Mother"],
-    correctOption: "Sister"
+    correctOption: "Sister",
   },
   {
     id: 11,
     question: "Urdu mein 'chand' kis cheez ko kehte hain?",
     options: ["Pahaad", "Phool", "Moon", "Raat"],
-    correctOption: "Moon"
+    correctOption: "Moon",
   },
   {
     id: 12,
     question: "Urdu mein 'khush' ka matlab kya hai?",
     options: ["Sad", "Angry", "Happy", "Tired"],
-    correctOption: "Happy"
+    correctOption: "Happy",
   },
   {
     id: 13,
     question: "Urdu mein 'bada' ka matlab kya hai?",
     options: ["Small", "Old", "Big", "Long"],
-    correctOption: "Big"
+    correctOption: "Big",
   },
   {
     id: 14,
     question: "Urdu mein 'shabash' ka matlab kya hai?",
     options: ["Sorry", "Good job", "I am sorry", "Hello"],
-    correctOption: "Good job"
-  }
+    correctOption: "Good job",
+  },
 ];
 
 export default urduQuestions;
